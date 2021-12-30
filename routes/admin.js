f5470a817ad67ad9d5d989021897e95e41943f63
bartbruneel <bartbruneelbart@gmail.com>
2021-12-30 19:37:32 +0100

    Adding html pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const path = require("path");
+
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Prodcut</button></form>'
-  );
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
-router.post("/product", (req, res) => {
+// /admin/add-product => POST
+router.post("/add-product", (req, res) => {
   console.log(req.body);
   res.redirect("/");
 });

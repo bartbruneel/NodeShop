8404ec877d594cf1c3db38f1265f384897d7acc8
bartbruneel <bartbruneelbart@gmail.com>
2022-01-04 21:39:03 +0100

    Try out pug and handlebars templating engines

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const path = require("path");
+const expressHbs = require("express-handlebars");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 
+app.engine(
+  "hbs",
+  expressHbs({
+    layoutsDir: "views/layouts",
+    defaultLayout: "main-layout",
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);

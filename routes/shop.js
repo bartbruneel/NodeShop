870649b5100d004907078ed418d049dc3c1999bd
bartbruneel <bartbruneelbart@gmail.com>
2022-01-15 18:15:53 +0100

    add editin product route and add to cart route

--- routes/shop.js
+++ routes/shop.js
@@ -11,6 +11,8 @@ router.get("/products", shopController.getProducts);
 
 router.get("/cart", shopController.getCart);
 
+router.post("/cart", shopController.postCart);
+
 router.get("/orders", shopController.getOrders);
 
 router.get("/products/:productId", shopController.getProduct);

11967a3de1fb0de8c67adb0daa7c21dc7338300c
bartbruneel <bartbruneelbart@gmail.com>
2022-01-23 19:53:13 +0100

    Add multiple products to cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -45,6 +45,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   let fetchedCart;
+  let newQuantity = 1;
   req.user
     .getCart()
     .then((cart) => {
@@ -56,17 +57,17 @@ exports.postCart = (req, res, next) => {
       if (products.length > 0) {
         product = products[0];
       }
-      let newQuantity = 1;
       if (product) {
-        // Do nothing
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
       }
-      return Product.findByPk(prodId)
-        .then((p) => {
-          return fetchedCart.addProduct(p, {
-            through: { quantity: newQuantity },
-          });
-        })
-        .catch((err) => console.log(err));
+      return Product.findByPk(prodId);
+    })
+    .then((p) => {
+      return fetchedCart.addProduct(p, {
+        through: { quantity: newQuantity },
+      });
     })
     .then(() => res.redirect("/cart"))
     .catch((err) => {

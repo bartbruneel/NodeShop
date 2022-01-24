2bda3572e4916e8a1e26102d6fb0eff61a77b833
bartbruneel <bartbruneelbart@gmail.com>
2022-01-24 19:06:55 +0100

    Add order page and route

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require("../models/product");
-const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -75,18 +74,17 @@ exports.postCart = (req, res, next) => {
     });
 };
 
-exports.getCheckout = (req, res, next) => {
-  res.render("/shop/checkout", {
-    path: "/checkout",
-    pageTitle: "Checkout",
-  });
-};
-
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
-  });
+  req.user
+    .getOrders({ include: ["products"] })
+    .then((orders) => {
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getProduct = (req, res, next) => {
@@ -118,5 +116,33 @@ exports.postCartDeleteProduct = (req, res, next) => {
     .then(() => {
       res.redirect("/cart");
     })
+    .catch((err) => console.log(err));
+};
+
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+  req.user
+    .getCart()
+    .then((cart) => {
+      fetchedCart = cart;
+      return cart.getProducts();
+    })
+    .then((products) => {
+      return req.user
+        .createOrder()
+        .then((order) => {
+          order.addProducts(
+            products.map((product) => {
+              product.orderItem = { quantity: product.cartItem.quantity };
+              return product;
+            })
+          );
+        })
+        .catch((err) => console.log(err));
+    })
+    .then((result) => {
+      return fetchedCart.setProducts(null);
+    })
+    .then(() => res.redirect("/orders"))
     .catch((err) => console.log(err));
 };
--- routes/shop.js
+++ routes/shop.js
@@ -17,8 +17,8 @@ router.get("/orders", shopController.getOrders);
 
 router.get("/products/:productId", shopController.getProduct);
 
-router.get("/checkout", shopController.getCheckout);
-
 router.post("/cart-delete-item", shopController.postCartDeleteProduct);
+
+router.post("/create-order", shopController.postOrder);
 
 module.exports = router;
